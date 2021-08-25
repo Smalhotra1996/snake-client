08ca913da601198fdc7c47c13e4177c5abf6ec5b
Sonam <sonam.malhotra2496@gmail.com>
2021-08-25 18:46:27 +0000

    Done connection module

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+const hello = function() {
+  console.log("hello");
+};
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.223.133",// IP address here,
+    
+    port:"50542"// PORT number here,
+    
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("hello there : Sonam ");
+  });
+  return conn;
+};
+
+
+
+module.exports = {
+  connect,
+  hello
+};
--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const net = require("net");
+const {connect,hello } = require('./client');
+
+
+hello();
+// console.log("Connecting ...");
+connect();
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.223.133",// IP address here,
-    
-    port:"50542"// PORT number here,
-    
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("hello there");
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
