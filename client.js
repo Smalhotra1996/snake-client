87f1360e165eaa24437e5e1ed7f1a0478ec1d590
Sonam <sonam.malhotra2496@gmail.com>
2021-08-26 17:06:26 +0000

    exported IP and PORT

--- client.js
+++ client.js
@@ -1,12 +1,16 @@
+
+const { IP, PORT } = require("./constanta");
+
+
 const net = require("net");
 const hello = function() {
   console.log("hello");
 };
 const connect = function() {
   const conn = net.createConnection({
-    host: "135.23.223.133",// IP address here,
+    host: IP,// IP address here,
     
-    port:"50542"// PORT number here,
+    port: PORT// PORT number here,
     
   });
 
@@ -18,12 +22,20 @@ const connect = function() {
 
   conn.setEncoding("utf8");
   conn.on("connect", () => {
-   setInterval(function(){ conn.write("Move: up");},1000); 
+    setInterval(function() {
+      conn.write("Move: up");
+    },1000);
     conn.write("Name: Sonam");
     conn.write("Hello everyone");
-    setInterval(function(){ conn.write("Move: down");},200);
-    setInterval(function(){ conn.write("Move: left");},500);
-    setInterval(function(){ conn.write("Move: right");},5800);
+    setInterval(function() {
+      conn.write("Move: down");
+    },200);
+    setInterval(function() {
+      conn.write("Move: left");
+    },500);
+    setInterval(function() {
+      conn.write("Move: right");
+    },5800);
   });
   return conn;
 };
@@ -32,5 +44,7 @@ const connect = function() {
 
 module.exports = {
   connect,
-  hello
+  hello,
+  IP,
+  PORT
 };
